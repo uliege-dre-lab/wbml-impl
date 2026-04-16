c74e2af8239fef7b157fef379a18b47f4ce11dad
Hearthlia <liu.elena.02@gmail.com>
2026-04-16 17:41:58 +0200

    Update: redesign class map for reusability

--- scripts/run_schema_converter.py
+++ scripts/run_schema_converter.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     run_schema_queries(
         source_file_path=Path("mappings/wbml_pokedex.ttl"),
         queries_dir=Path("src/wikibase_pipeline/sparql/Schema"),
-        output_path=Path("data/output/schema_pokedex.ttl"),
+        output_file_path=Path("data/output/schema_pokedex.ttl"),
         verbose=2,
     )
