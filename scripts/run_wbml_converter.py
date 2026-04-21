dcc204aa8cf9fa68a472d0a448821ee934546189
Hearthlia <liu.elena.02@gmail.com>
2026-04-21 17:18:28 +0200

    Add:Mapping for json pokemon

--- scripts/run_wbml_converter.py
+++ scripts/run_wbml_converter.py
@@ -4,8 +4,8 @@ from wikibase_pipeline.wbml_to_rml import convert_wbml_to_rml
 
 if __name__ == "__main__":
     convert_wbml_to_rml(
-        mapping_file_path=Path("mappings/wbml_pokedex.ttl"),
+        mapping_file_path=Path("data/mappings/wbml_pokemon.ttl"),
         queries_dir=Path("src/wikibase_pipeline/sparql/rml"),
-        output_file_path=Path("mappings/rml_pokedex_converted.ttl"),
+        output_file_path=Path("data/mappings/rml_pokemon_converted.ttl"),
         verbose=2,
     )
