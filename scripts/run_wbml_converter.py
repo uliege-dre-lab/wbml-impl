2826d1bf272d2ff2b90f5a34d64cf7a705becf37
Hearthlia <liu.elena.02@gmail.com>
2026-05-03 16:00:47 +0200

    Update: use wbml:template in mapping to avoid conflicting labels

--- scripts/run_wbml_converter.py
+++ scripts/run_wbml_converter.py
@@ -4,7 +4,7 @@ from wikibase_pipeline.wbml_to_rml import convert_wbml_to_rml
 
 if __name__ == "__main__":
     convert_wbml_to_rml(
-        mapping_file_path=Path("data/mappings/wbml_notion.ttl"),
-        output_file_path=Path("data/mappings/rml_notion_converted.ttl"),
+        mapping_file_path=Path("data/mappings/wbml_pokemon.ttl"),
+        output_file_path=Path("data/mappings/rml_pokemon_converted.ttl"),
         verbose=2,
     )
