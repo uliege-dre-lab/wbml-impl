12224c30da0b62611fb35caa8a57d598178774e5
Hearthlia <liu.elena.02@gmail.com>
2026-05-04 16:35:50 +0200

    Fix:rename output file path

--- demonstrators/scripts/run_schema_converter.py
+++ demonstrators/scripts/run_schema_converter.py
@@ -5,6 +5,6 @@ from wikibase_pipeline.wbml_to_rml import run_schema_queries
 if __name__ == "__main__":
     run_schema_queries(
         source_file_path=Path("demonstrators/pokemon/mappings/wbml_pokemon.ttl"),
-        output_file_path=Path("demonstrators/pokemon/output/schema_pokemon.ttl"),
+        output_file_path=Path("demonstrators/pokemon/output/pokemon_schema.ttl"),
         verbose=2,
     )
